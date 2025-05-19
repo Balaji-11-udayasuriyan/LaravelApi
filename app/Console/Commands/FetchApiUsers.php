abee754c99677534c9403ddf96f576e20f1de6da
Balaji Udayasuiryan <balajiudayasuriyan1102@gmail.com>
2025-05-19 19:07:23 +0530

    add FetchApiUsers logic

---
+++ app/Console/Commands/FetchApiUsers.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\UserApi;
+use Illuminate\Support\Facades\Http;
+
+class FetchApiUsers extends Command
+{
+    protected $signature = 'fetch:api-users';
+    protected $description = 'Fetch and store users from JSONPlaceholder API';
+
+    public function handle()
+    {
+        $response = Http::get('https://jsonplaceholder.typicode.com/users');
+        if ($response->successful()) {
+            $users = $response->json();
+            foreach ($users as $user) {
+                UserApi::updateOrCreate(
+                    ['email' => $user['email']],
+                    [
+                        'name' => $user['name'],
+                        'street' => $user['address']['street'],
+                        'city' => $user['address']['city'],
+                    ]
+                );
+            }
+            $this->info('Users fetched and stored successfully!');
+        } else {
+            $this->error('Failed to fetch users from API');
+        }
+    }
+}
